aa87419ac2d4af3c13fdd4e4b493f5530ec11ecd
Manto123-1 <caiyuha@kean.edu>
2026-09-23 21:58:04 -0400

    Calculate drink and pastry revenue

--- assignment1_salesAnalysis/analysis.py
+++ assignment1_salesAnalysis/analysis.py
@@ -3,3 +3,8 @@ drink_price= 3.0
 drinks_sold= 143
 pastries_price= 3.5
 pastry_sold= 33
+
+#compute revenue
+drink_revenue= drinks_sold*drink_price
+pastry_revenue=pastry_sold*pastries_price
+total_revenue= drink_revenue+pastry_revenue
